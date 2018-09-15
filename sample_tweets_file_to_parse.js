62b18dd4b95ee7f0f670945ef2233542606f8f3d
jyllsarta <illstarred.dive@gmail.com>
2018-09-15 23:00:35 +0900

    jsとaccess_keysのサンプルを追加

---
+++ sample_tweets_file_to_parse.js
@@ -0,0 +1,39 @@
+window.YTD.tweet.part0 = [{
+    "retweeted": false,
+    "source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+    "entities": {
+        "hashtags": [],
+        "symbols": [],
+        "user_mentions": [],
+        "urls": []
+    },
+    "display_text_range": ["0", "58"],
+    "favorite_count": "0",
+    "id_str": "1039848778742681601",
+    "truncated": false,
+    "retweet_count": "0",
+    "id": "1039848778742681601",
+    "created_at": "Wed Sep 12 12:10:42 +0000 2018",
+    "favorited": false,
+    "full_text": "sudoつけないとshutdownもrebootもcommand not foundに見えるんかい！ なんじゃそりゃ",
+    "lang": "ja"
+}, {
+    "retweeted": false,
+    "source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+    "entities": {
+        "hashtags": [],
+        "symbols": [],
+        "user_mentions": [],
+        "urls": []
+    },
+    "display_text_range": ["0", "4"],
+    "favorite_count": "0",
+    "id_str": "1039845159553265664",
+    "truncated": false,
+    "retweet_count": "0",
+    "id": "1039845159553265664",
+    "created_at": "Wed Sep 12 11:56:20 +0000 2018",
+    "favorited": false,
+    "full_text": "なんなん",
+    "lang": "ja"
+}]
